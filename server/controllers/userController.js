00d3ca01b47fe428caae3ab76ec2c3dd7000e669
stailzgit <77897064+stailzgit@users.noreply.github.com>
2022-01-27 22:57:47 +0300

    login JWT

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,15 @@
 const ApiError = require('../error/ApiError')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const {User, Basket} = require('../models/models')
+
+const generateJWT = (id, email, role) => {
+  return jwt.sign(
+    {id: id, email, role},
+    process.env.SECRET_KEY,
+    {expiresIn: '24h'}
+  )
+}
 
 class UserController {
   async registration(req, res, next) {
@@ -16,19 +25,30 @@ class UserController {
 
     const hashPassword = await  bcrypt.hash(password, 5)
     const user = await User.create({email, role, password: hashPassword})
-
+    const basket = await Basket.create({userId: user.id})
+    const token = generateJWT(user.id, user.email, user.role)
+    return res.json({token})
   }
 
-  async login(req, res) {
+  async login(req, res, next) {
+    const {email, password} = req.body
+    const user = await User.findOne({where: {email}})
+    if(!User) {
+      return next(ApiError.internal('Пользователь с таким именем не найден'))
+    }
 
+    let comparePassword = bcrypt.compareSync(password, user.password)
+    if(!comparePassword){
+      return next(ApiError.internal('Указан неверный пароль'))
+    }
+
+    const token = generateJWT(user.id, user.email, user.role)
+
+    return res.json({token})
   }
 
   async check(req, res, next) {
-    const {id} = req.query
-    if(!id){
-      return next(ApiError.badRequest('Не задан ID'))
-    }
-    res.json(id)
+    //
   }
 }
 
